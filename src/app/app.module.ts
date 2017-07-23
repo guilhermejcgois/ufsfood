538a3cf6be20902afa9c93bf9e4735f05e74f656
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2017-07-23 13:36:38 -0300

    Adds circleci conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-/* Angular Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCACdCvtf9-v9EF6_2HJdP0-6HuvTp9KnY',
+    authDomain: 'ufsfood-ce233.firebaseapp.com',
+    databaseURL: 'https://ufsfood-ce233.firebaseio.com',
+    projectId: 'ufsfood-ce233',
+    storageBucket: 'ufsfood-ce233.appspot.com',
+    messagingSenderId: '62531985938'
+  }
 };
